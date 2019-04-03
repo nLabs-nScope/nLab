d07663a492824850917beff3966f10d2ce7349b7
David Meyer <david@nscope.org>
2019-04-02 20:44:45 -0500

    finish implementing power monitor

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -5,22 +5,49 @@ window.Bootstrap = require('bootstrap')
 const path = require("path");
 const nScopeAPI = require(path.resolve('app/js/nScopeAPI'));
 
-function updatePowerUsage(usage)
+function updatePowerUsage(state, usage)
 {
-    if(usage < 0)
+    switch(state)
     {
-        $("#usb-status-bar, #usb-status").css("visibility","hidden");
-        $(".nscope-usb-disconnected").css("visibility","visible");
-        updatePowerUsage.percentage = null;
-    }
-    else
-    {
-        $(".nscope-usb-disconnected").css("visibility","hidden");
-        $("#usb-status-bar, #usb-status").css("visibility","visible");
-        var percentage = usage*100/2.5;
-        updatePowerUsage.percentage = updatePowerUsage.percentage*0.8+percentage*0.2 || percentage;
-        $(".nscope-power-usage-bar .nscope-power-usage").css("width",updatePowerUsage.percentage+'%');
-        $('#usb-status-bar, #usb-status').html((updatePowerUsage.percentage/100*2.5).toFixed(2)+' W');
+        case 0:
+        {
+            $("#usb-status-bar, #usb-status").css("visibility","hidden");
+            $(".nscope-usb-disconnected").css("visibility","hidden");
+            $(".nscope-usb-power-fault").css("visibility","hidden");
+            $(".nscope-usb-power-off").css("visibility","visible");
+            updatePowerUsage.percentage = null;
+            break;
+        }
+        case 1:
+        {
+            $(".nscope-usb-disconnected").css("visibility","hidden");
+            $(".nscope-usb-power-off").css("visibility","hidden");
+            $(".nscope-usb-power-fault").css("visibility","hidden");
+            $("#usb-status-bar, #usb-status").css("visibility","visible");
+            var percentage = usage*100/2.5;
+            updatePowerUsage.percentage = updatePowerUsage.percentage*0.8+percentage*0.2 || percentage;
+            $(".nscope-power-usage-bar .nscope-power-usage").css("width",updatePowerUsage.percentage+'%');
+            $('#usb-status-bar, #usb-status').html((updatePowerUsage.percentage/100*2.5).toFixed(2)+' W');
+            break;
+        }
+        case 2:
+        {
+            $("#usb-status-bar, #usb-status").css("visibility","hidden");
+            $(".nscope-usb-disconnected").css("visibility","hidden");
+            $(".nscope-usb-power-off").css("visibility","hidden");
+            $(".nscope-usb-power-fault").css("visibility","visible");
+            updatePowerUsage.percentage = null;
+            break;
+        }
+        default:
+        {
+            $("#usb-status-bar, #usb-status").css("visibility","hidden");
+            $(".nscope-usb-power-off").css("visibility","hidden");
+            $(".nscope-usb-power-fault").css("visibility","hidden");
+            $(".nscope-usb-disconnected").css("visibility","visible");
+            updatePowerUsage.percentage = null;
+            break;
+        }
     }
     return usage;
 }
@@ -37,10 +64,13 @@ function monitorScope(){
     }
     else
     {
-        if(updatePowerUsage(nScopeAPI.get_power_usage()) < 0)
+        let state = nScopeAPI.get_power_state();
+        let usage = nScopeAPI.get_power_usage();
+        if(state < 0 || usage < 0)
         {
             monitorScope.isOpen = false;
         }
+        updatePowerUsage(state, usage);
     }
 
 }
