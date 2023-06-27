515620efb0f0485236a38a5a0e5a53b1f2c70752
David Meyer <david@nscope.org>
2023-06-26 23:13:17 -0700

    lets try building universal

--- forge.config.js
+++ forge.config.js
@@ -9,8 +9,10 @@ module.exports = {
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: '@electron-forge/maker-dmg',
+      config: {
+        format: 'ULFO',
+      }
     },
     {
       name: '@electron-forge/maker-deb',
