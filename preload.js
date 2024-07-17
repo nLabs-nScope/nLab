0d8043c56802955c8bfe74138b8175b716290b3d
David Meyer <david@nscope.org>
2024-07-17 00:43:12 -0700

    log in the preload script too

--- preload.js
+++ preload.js
@@ -1,2 +1,7 @@
-window.nscope = require('./app/nscope.node')
+const electron_log = require('electron-log');
+const log = electron_log.scope("preload");
+log.info("starting preload");
+window.nscope = require('./app/nscope.node');
+log.info("loaded native node module");
 window.nScope = nscope.newNscope();
+log.info("created new nScope");
