525d01897e63b71f342fdeabbd547178620f3338
David Meyer <david@nscope.org>
2024-07-17 22:33:31 -0700

    fix the loading path

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const electron_log = require('electron-log');
 electron_log.transports.console.level = false;
 const log = electron_log.scope("preload");
 log.info("starting preload");
-window.nscope = require('./app/foo/nscope.node');
+window.nscope = require('./app/nscope.node');
 log.info("loaded native node module");
 window.nScope = nscope.newNscope();
 log.info("created new nScope");
