59d880caa9456ed04a8c9241347fd9ad92ab5201
David Meyer <david@nscope.org>
2024-07-17 21:26:06 -0700

    test of bad path

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 const electron_log = require('electron-log');
 const log = electron_log.scope("preload");
 log.info("starting preload");
-window.nscope = require('./app/nscope.node');
+window.nscope = require('./app/foo/nscope.node');
 log.info("loaded native node module");
 window.nScope = nscope.newNscope();
 log.info("created new nScope");
