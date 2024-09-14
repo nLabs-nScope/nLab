9589b6f2c3f72ee7a554b4f6898d08f544e7fb18
David Meyer <david@nscope.org>
2024-09-13 21:29:06 -0700

    factor out common code to module

---
+++ src/js/PrecisionInput.js
@@ -0,0 +1,71 @@
+function button_from_label(element) {
+    let nextElement = element.nextElementSibling; // Start with the next sibling
+
+    // Loop until you find an <i> tag or run out of siblings
+    while (nextElement) {
+        if (nextElement.tagName.toLowerCase() === 'i') {
+            return nextElement; // Found the first <i> tag
+        }
+        nextElement = nextElement.nextElementSibling; // Move to the next sibling
+    }
+
+    return null; // No <i> tag found
+}
+
+function start_edit(label, button) {
+    label.setAttribute("contenteditable", true);
+    label.classList.add("editing");
+    button.classList.add("editing");
+    button.classList.remove("fa-regular", "fa-pen-to-square");
+    button.classList.add("fa-solid", "fa-check");
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNodeContents(label);
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
+
+function end_edit(label, button) {
+    label.setAttribute("contenteditable", false);
+    label.classList.remove("editing");
+    button.classList.remove("editing");
+    button.classList.remove("fa-solid", "fa-check");
+    button.classList.add("fa-regular", "fa-pen-to-square");
+}
+
+export function setup(label) {
+
+    let button = button_from_label(label);
+
+    label.onclick = function () {
+        start_edit(label, button);
+    }
+
+    label.onkeydown = function (event) {
+        const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-'];
+        // Allow backspace, delete, etc.
+        if (event.key === 'Backspace' || event.key === 'Delete' || event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+            return;
+        }
+        // Prevent input if the key is not allowed
+        if (!allowedKeys.includes(event.key)) {
+            event.preventDefault();
+        }
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            end_edit(label, button)
+        }
+    }
+
+    button.onclick = function () {
+        if(this.classList.contains("editing")){
+            end_edit(label, button);
+        } else {
+            start_edit(label, button);
+        }
+    }
+}
+
+export function isNotEditable(label) {
+    return !label.classList.contains("editing");
+}
