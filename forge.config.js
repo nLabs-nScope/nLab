8c4c54f2db3c1e7d9d5e93cca4e26bf3cd517e0b
David Meyer <10246856+davidjmeyer@users.noreply.github.com>
2024-09-25 23:15:53 -0700

    Stop signing development builds (#91)
    Only trigger code signing when on the main branch or is explicitly tagged

--- forge.config.js
+++ forge.config.js
@@ -1,56 +1,59 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-    icon: 'src/assets/icons/nscope_icon',
-    appBundleId: 'org.nscope.nscopeapp',
-    osxSign: {
-      identity: "W74MCWRPFC",
-      hardenedRuntime: true,
+    packagerConfig: {
+        asar: true,
+        icon: 'src/assets/icons/nscope_icon',
+        appBundleId: 'org.nscope.nscopeapp',
     },
-    osxNotarize: {
-      tool: 'notarytool',
-      appleId: process.env.MACOS_NOTARIZATION_APPLE_ID,
-      appleIdPassword: process.env.MACOS_NOTARIZATION_PWD,
-      teamId: process.env.MACOS_NOTARIZATION_TEAM_ID
-    }
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        setupExe: 'nScope Installer.exe',
-        loadingGif: 'src/assets/gif/installing.gif',
-        iconUrl: 'https://www.nscope.org/user/pages/icons/nscope_icon.ico',
-        setupIcon: 'src/assets/icons/nscope_icon.ico',
-        signWithParams: `/csp "DigiCert Software Trust Manager KSP" /kc key_847858243 /f ${process.env.CODE_SIGNING_CERT_FILE} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256`
-      },
-    },
-    {
-      name: '@electron-forge/maker-dmg',
-      config: {
-        format: 'ULFO',
-        icon: 'src/assets/icons/nscope_icon.icns',
-        name: 'nScope Installer',
-      }
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                setupExe: 'nScope Installer.exe',
+                loadingGif: 'src/assets/gif/installing.gif',
+                iconUrl: 'https://www.nscope.org/user/pages/icons/nscope_icon.ico',
+                setupIcon: 'src/assets/icons/nscope_icon.ico',
+            },
+        },
+        {
+            name: '@electron-forge/maker-dmg',
+            config: {
+                format: 'ULFO',
+                icon: 'src/assets/icons/nscope_icon.icns',
+                name: 'nScope Installer',
+            }
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            config: {},
+        },
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-auto-unpack-natives',
+            config: {},
+        },
+    ],
 };
+
+if (process.env.NSCOPE_CODESIGN) {
+    module.exports.packagerConfig.osxNotarize = {
+        tool: 'notarytool',
+        appleId: process.env.MACOS_NOTARIZATION_APPLE_ID,
+        appleIdPassword: process.env.MACOS_NOTARIZATION_PWD,
+        teamId: process.env.MACOS_NOTARIZATION_TEAM_ID
+    }
+    module.exports.packagerConfig.osxSign = {
+        identity: "W74MCWRPFC",
+        hardenedRuntime: true,
+    }
+    module.exports.makers[0].config.signWithParams = `/csp "DigiCert Software Trust Manager KSP" /kc key_847858243 /f ${process.env.CODE_SIGNING_CERT_FILE} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256`
+}
