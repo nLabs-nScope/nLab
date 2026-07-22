fa7193294d8064bdcdba0ee376ca8b3537a16638
David Meyer <david@nscope.org>
2026-07-21 23:17:15 -0700

    ensure appimage builder complies with MakerBase

--- forge.config.js
+++ forge.config.js
@@ -1,4 +1,18 @@
-const path = require('path');
+const { MakerBase } = require('@electron-forge/maker-base');
+
+class MakerAppImage extends MakerBase {
+    name = 'appimage';
+    defaultPlatforms = ['linux'];
+
+    isSupportedOnCurrentPlatform() {
+        return true;
+    }
+
+    async make(options) {
+        const { buildForge } = require('app-builder-lib');
+        return buildForge(options, { linux: [`appimage:${options.targetArch}`] });
+    }
+}
 
 module.exports = {
     packagerConfig: {
@@ -48,14 +62,11 @@ module.exports = {
                 }
             }
         },
-        {
-            name: 'electron-forge-maker-appimage',
-            config: {
-                options: {
-                    icon: 'src/assets/icons/nLabApp_Icon_512x512@2x.png',
-                }
+        new MakerAppImage({
+            options: {
+                icon: 'src/assets/icons/nLabApp_Icon_512x512@2x.png',
             }
-        },
+        }),
         {
             name: '@electron-forge/maker-zip',
             config: {},
